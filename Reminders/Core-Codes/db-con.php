8766ac5e0aea06f6d87f1dc8550d3c5d9966aafb
rockboy69 <sofian.joosab@gmail.com>
2018-06-20 08:57:28 +1000

    update DB name

--- Reminders/Core-Codes/db-con.php
+++ Reminders/Core-Codes/db-con.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$conn = mysqli_connect("localhost", "root", "", "meucv");
+	$conn = mysqli_connect("localhost", "root", "", "Reminders");
 
 	if(!$conn){
 		die("connection failed: ".mysqli_connect_error());
